8cdd828ddf267c39d7e40345862a8216b34d549d
ForeaFactor <76183563+ForeaFactor@users.noreply.github.com>
2024-12-09 04:08:21 +0100

    feat(day08): started work on tsk 2 (new Antinode generator) but wrong answer

--- day_08/main.go
+++ day_08/main.go
@@ -9,15 +9,20 @@ import (
 )
 
 func Main() {
-	input := readInput("./day_08/input.txt")
-	locationsMap := generateAntennasGridFromText(input)
-	antinodes := generateAntinodes(&locationsMap)
-	locationsMap.addPoiAntinodes(antinodes)
+	input := readInput("./day_08/sample.txt")
+	gridTsk1 := generateAntennasGridFromText(input)
+	antinodes := generateAntinodesModelTsk1(&gridTsk1)
+	gridTsk1.addPoiAntinodes(antinodes)
+
+	gridTsk2 := generateAntennasGridFromText(input)
+	antinodes = generateAntinodesModelTsk2(&gridTsk2)
+	gridTsk2.addPoiAntinodes(antinodes)
 
 	fmt.Printf("\n====== DAY 04 ======\n")
-	fmt.Printf("%d = Number of Antinodes\n", countAntinodes(locationsMap))
+	fmt.Printf("%d = Number of Antinodes\n", countAntinodes(gridTsk1))
+	fmt.Printf("%d = Number of Antinodes corrected Model (Tsk2)\n", countAntinodes(gridTsk2))
 
-	fmt.Printf("%s", locationsMap.exportGridToText())
+	fmt.Printf("%s", gridTsk2.exportGridToText())
 }
 
 type poi interface {
@@ -105,6 +110,15 @@ func (g *grid) getPoiByPos(c cord) *poi {
 	return &point
 }
 
+func (g *grid) containsCord(c cord) bool {
+	// method to check if coordinates are in bound of the grid
+	if isIntBetween(0, g.width, c.x) && isIntBetween(0, g.height, c.y) {
+		// inlcuding lower and excluding upper
+		return true
+	}
+	return false
+}
+
 func (a poiAntenna) getPos() cord {
 	return a.pos
 }
@@ -117,11 +131,7 @@ func (a poiAntenna) getIcon() byte {
 
 func (a poiAntenna) isAllowedToExistIn(g *grid) bool {
 	// multiple antennas at same Coordinates are allowed
-	if isIntBetween(0, g.width, a.pos.x) && isIntBetween(0, g.height, a.pos.y) {
-		// inlcuding lower and excluding upper
-		return true
-	}
-	return false
+	return g.containsCord(a.pos)
 }
 
 func (a poiAntinode) getPos() cord {
@@ -135,8 +145,7 @@ func (a poiAntinode) getIcon() byte {
 func (a poiAntinode) isAllowedToExistIn(g *grid) bool {
 	// [ ] multiple antinodes of the same freqency are not allowed
 	// [x] muliple antinodes per position are not allowed at all
-	if false == (isIntBetween(0, g.width, a.pos.x) && isIntBetween(0, g.height, a.pos.y)) {
-		// inlcuding lower and excluding upper
+	if false == g.containsCord(a.pos) {
 		return false
 	}
 	existingPois := g.pois[a.pos]
@@ -180,7 +189,7 @@ func newPoiAntinode(pos cord, sources [2]*poi) *poiAntinode {
 	}
 }
 
-func generateAntinodes(g *grid) []poiAntinode {
+func generateAntinodesModelTsk1(g *grid) []poiAntinode {
 	// identify all unique frequencies
 	freqSet := make(map[byte]struct{})
 	for _, points := range g.pois {
@@ -226,6 +235,50 @@ func generateAntinodes(g *grid) []poiAntinode {
 	return newAntinodes
 }
 
+func generateAntinodesModelTsk2(g *grid) []poiAntinode {
+	// identify all unique frequencies
+	freqSet := make(map[byte]struct{})
+	for _, points := range g.pois {
+		for _, point := range points {
+			switch i := point.(type) {
+			case poiAntenna:
+				antenna := poiAntenna(i)
+				freqSet[antenna.freq] = struct{}{}
+			}
+		}
+	}
+
+	var newAntinodes []poiAntinode
+	//it's dirty to assume the freq is same as Icon but, need to change to getPoiAntennasByFreq()
+	for freq, _ := range freqSet { // only antennas of same frequency can generate antinodes
+		sameFreqAntennas := g.getPoisByIcon(freq)
+		for idx, antennaOne := range sameFreqAntennas {
+			for _, antennaTwo := range sameFreqAntennas[idx+1:] {
+				// In particular, an antinode occurs at any point that is perfectly in line with two antennas of the same frequency
+				// but only when one of the antennas is twice as far away as the other
+				// hate these type switches -- so ugly -- only the antPosLeft assignment is importend
+				switch one := (*antennaOne).(type) {
+				case poiAntenna:
+					switch two := (*antennaTwo).(type) {
+					case poiAntenna:
+						// Direciton [ant1]----[ant2]--->[node]--->[node]--->[node]
+						var antPositions []cord = calcPosOfAntinodes(one, two, *g)
+						for _, position := range antPositions {
+							newAntinodes = append(newAntinodes, *newPoiAntinode(position, [2]*poi{antennaOne, antennaTwo}))
+						}
+						// Direction [node]<---[node]<---[node]<---[ant1]----[ant2]
+						antPositions = calcPosOfAntinodes(two, one, *g)
+						for _, position := range antPositions {
+							newAntinodes = append(newAntinodes, *newPoiAntinode(position, [2]*poi{antennaTwo, antennaOne}))
+						}
+					}
+				}
+			}
+		}
+	}
+	return newAntinodes
+}
+
 func (g *grid) getPoisByIcon(icon byte) []*poi {
 	result := make([]*poi, 0)
 	for _, pois := range g.pois {
@@ -249,8 +302,6 @@ func readInput(name string) []byte {
 	return data
 }
 
-//---------functions declaration---------
-
 func generateAntennasGridFromText(input []byte) grid {
 	// assume ascii Encoding
 	var out grid
@@ -271,6 +322,8 @@ func generateAntennasGridFromText(input []byte) grid {
 	return out
 }
 
+//---------functions declaration---------
+
 func countAntinodes(g grid) uint {
 	var count uint = 0
 	for _, pois := range g.pois {
@@ -290,13 +343,36 @@ func isIntBetween(lower int, upper int, x int) bool {
 }
 
 func calcPosOfAntinode(one poiAntenna, two poiAntenna) cord {
+	// Task One Antinode position calc
 	// [ant]----[ant]--->[node]
-	//  one	  two	   newAntinode
+	//  one	     two	      newAntinode
 	distanceOfSources := manhattanDistance(one.pos, two.pos)
 	return cord{
 		x: two.pos.x + distanceOfSources.x,
 		y: two.pos.y + distanceOfSources.y,
 	}
+}
+
+func calcPosOfAntinodes(one poiAntenna, two poiAntenna, g grid) []cord {
+	// Task Two Antinode position calc TODO: make the naming difference between the calcPos..() functions more clear
+	// [ant]----[ant]--->[node]--->[node]--->[node]--->[node]--->[node]--->[node]--->[node]--->||Border
+	//  one	     two	  newAntinodes
+	// need grid to determine the boundaries
+	distanceOfSources := manhattanDistance(one.pos, two.pos)
+	newCordsList := make([]cord, 0)
+	distanceMultiplier := 1
+	for {
+		nextCord := cord{
+			x: two.pos.x + distanceOfSources.x*distanceMultiplier,
+			y: two.pos.y + distanceOfSources.y*distanceMultiplier,
+		}
+		if !g.containsCord(nextCord) {
+			break
+		}
+		distanceMultiplier++
+		newCordsList = append(newCordsList, nextCord)
+	}
+	return newCordsList
 }
 
 func manhattanDistance(posOne cord, posTwo cord) cord {
--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"aoc24/day_01"
+	"aoc24/day_02"
+	"aoc24/day_03"
+	"aoc24/day_04"
 	"aoc24/day_08"
 )
 
 func main() {
-	/*	day_01.Main()
-		day_02.Main()
-		day_03.Main()
-		day_04.Main()
-	*/day_08.Main()
+	day_01.Main()
+	day_02.Main()
+	day_03.Main()
+	day_04.Main()
+	day_08.Main()
 }
