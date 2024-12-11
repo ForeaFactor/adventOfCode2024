a4d1d5058c5a81cd3e4fec8cfb883d1adb4e0c43
ForeaFactor <76183563+ForeaFactor@users.noreply.github.com>
2024-12-11 21:37:31 +0100

    chore(day_09): store tsk2 in progres - lots of debuging

--- day_09/main.go
+++ day_09/main.go
@@ -1,26 +1,25 @@
 package day_09
 
 import (
-	"crypto/cipher"
 	"fmt"
 	"io"
 	"log"
 	"os"
-	"strconv"
 )
 
 func Main() {
-	input := readInput("./day_09/input.txt")
+	input := readInput("./day_09/sample.txt")
 	diskMap := interpretBytesAsNumerals(input)
 	diskTsk1 := newStorageFromDiskMap(diskMap)
-	diskTsk1.compactTsk1()
+	//diskTsk1.compactTsk1()
 	diskTsk2 := newStorageFromDiskMap(diskMap)
+	diskTsk2.updateLookup()
 	diskTsk2.compactTsk2()
 
 	fmt.Printf("\n====== DAY 8 ======\n")
 	fmt.Printf("%d = Checksum Of File After Compacting\n", diskTsk1.checksum())
 	fmt.Printf("%d = Checksum Of File After Compacting Better\n", diskTsk2.checksum())
-	fmt.Println(string(diskTsk1.exportDataToText()))
+	//fmt.Println(string(diskTsk2.exportDataToText()))
 
 }
 
@@ -41,7 +40,7 @@ type datapoint struct {
 type block struct {
 	start  int
 	length int
-	datapoint
+	data   datapoint
 }
 
 func newStorageFromDiskMap(in []byte) storage {
@@ -78,9 +77,9 @@ func (s *storage) exportDataToText() string {
 	for _, dataPoint := range s.data {
 		switch dataPoint.typ {
 		case 0:
-			out += "."
+			out += ". "
 		case 1:
-			out += "[" + strconv.Itoa(dataPoint.id) + "]" // this results in wierd characters
+			out += fmt.Sprintf("%02d", dataPoint.id)
 		}
 	}
 	return out
@@ -108,13 +107,69 @@ func (s *storage) compactTsk1() {
 }
 
 func (s *storage) compactTsk2() {
-	// to Tsk2
-
+	// to Tsk2 move file blocks one at a time from the end of the disk to the leftmost free space block
+	// move each file exactly once and if there is not enough free space the file is left there
+	currBlock, err := s.getBlockAt(len(s.data) - 1)
+	if err != nil {
+		panic(err)
+	}
+	for err != io.EOF {
+		// the curr block is retrieved at the end of each loop iteration
+		//s.updateLookup()
+		//fmt.Println(s.exportLookupToText())
+		fmt.Println(s.exportDataToText())
+		if currBlock.data.typ == 0 {
+			// id == 0 -> empty block -> dont process
+			currBlock, err = s.nextBlock(currBlock.start, -1)
+			continue
+		}
+		if destAddr, _ := s.allocateSpace(currBlock.length); destAddr != -1 {
+			// value '-1' signals there is no free space
+			if destAddr+currBlock.length > currBlock.start {
+				// destAddr must be left of oldBlock according to tsk2 so skip instead
+				currBlock, err = s.nextBlock(currBlock.start, -1)
+				continue
+			}
+			err = s.moveBlock(currBlock, destAddr)
+			if err != nil {
+				panic(err)
+			}
+		} else {
+			// no free space found -> skip that block
+			// TODO process the error of 's.allocateSpace' somehow
+			currBlock, err = s.nextBlock(currBlock.start, -1)
+			continue
+		}
+		currBlock, err = s.nextBlock(currBlock.start, -1)
+	}
 }
 
 func (s *storage) updateLookup() {
 	// to Tsk2
+	newLookup := make([]block, 0)
+	currBlock, err := s.getBlockAt(0)
+	if err != nil {
+		panic(err)
+	}
+	for err != io.EOF {
+		newLookup = append(newLookup, currBlock)
+		startOfNextBlock := currBlock.start + currBlock.length
+		currBlock, err = s.getBlockAt(startOfNextBlock)
+		if err != nil && err != io.EOF {
+			panic(err)
+		}
+	}
+	// TODO: implement lookup validity and consistency check
+	s.lookup = newLookup
 
+}
+
+func (s *storage) exportLookupToText() string {
+	var out string = ""
+	for _, currBlock := range s.lookup {
+		out += fmt.Sprintf("%d", currBlock.length)
+	}
+	return out
 }
 
 func (s *storage) nextDatapoint(iter *int, typ byte) (datapoint, error) {
@@ -160,33 +215,95 @@ func (s *storage) allocateSpace(size int) (int, error) {
 				var startOfFreeBlock int = i - freeBlockLen
 				return startOfFreeBlock, nil
 			}
-			freeBlockLen = 0
+			freeBlockLen = 0 // reset free-counter
 		}
 	}
 	return -1, fmt.Errorf("no free space of size %d", size)
 }
 
-func (s *storage) nextBlockOfDataPoints(start int, typ byte, direction int) (int, int, error) {
-	// returns start and length of the block found - can search towards the left (-1) or right (1) of the start
-	// TODO: restric direction to be either -1 or 1
-	var blockLen int = 0
-	var currBlockDP datapoint = s.data[start]
-	for i := start; i < len(s.data) && i > 0; i += direction {
-		if s.data[i] == currBlockDP {
-			blockLen++
-		} else {
-			// block ended since the datapoint changed
-			var blockStart int // defaults to 0
-			if direction > 0 {
-				blockStart = i - blockLen
-			}
-			if direction < 0 {
-				blockStart = i + 1
-			}
-			return blockStart, blockLen, nil
+func (s *storage) writeBlock(b block, destAddr int) error {
+	// TODO return value: number of datapoints written
+	if destAddr < 0 || (destAddr+b.length) > len(s.data) {
+		return fmt.Errorf("destination address-range out of bound")
+	}
+	if b.start < 0 || (b.start+b.length) > len(s.data) {
+		return fmt.Errorf("source block address range out of bound")
+	}
+	for i := 0; i < b.length; i++ {
+		s.data[destAddr+i] = b.data
+	}
+	return nil
+}
+
+func (s *storage) moveBlock(b block, destAddr int) error {
+	err := s.writeBlock(b, destAddr)
+	emptyBlock := block{
+		start:  b.start,
+		length: b.length,
+		data:   *new(datapoint),
+	}
+	if err != nil {
+		return err
+	}
+	err = s.writeBlock(emptyBlock, b.start)
+	return err
+}
+
+func (s *storage) getBlockAt(start int) (block, error) {
+	if start > len(s.data)-1 || start < 0 {
+		// on error return empty block
+		return *new(block), io.EOF
+	}
+	var refferenceDP datapoint = s.data[start] // the datapoint the block contains
+	var rightBorder int = start
+	var leftBorder int = start
+	for rightBorder = start; true; rightBorder++ {
+		if rightBorder > len(s.data)-1 {
+			rightBorder--
+			break
+		}
+		if s.data[rightBorder] != refferenceDP {
+			rightBorder-- // since in last iteration the start of the next block was found
+			break
 		}
 	}
+	for leftBorder = start; true; leftBorder-- {
+		if leftBorder < 0 {
+			leftBorder++
+			break
+		}
+		if s.data[leftBorder] != refferenceDP {
+			leftBorder++ // since in last iteration the end of the prev block was found
+			break
+		}
+	}
+	return block{
+		start:  leftBorder,
+		length: rightBorder - leftBorder + 1,
+		data:   refferenceDP,
+	}, nil
+}
 
+func (s *storage) nextBlock(pos int, direction int) (block, error) {
+	// TODO: restric direction to be either -1 or 1
+	var result block = *new(block)
+	currBlock, err := s.getBlockAt(pos)
+	if err != nil {
+		return result, err
+	}
+	switch direction {
+	case -1:
+		result, err = s.getBlockAt(currBlock.start - 1)
+		if err != nil {
+			return result, err
+		}
+	default:
+		result, err = s.getBlockAt(currBlock.start + currBlock.length)
+		if err != nil {
+			return result, err
+		}
+	}
+	return result, err
 }
 
 func (s *storage) swapDatapoints(front int, back int) {
@@ -201,8 +318,8 @@ func (s *storage) checksum() uint64 {
 	for _, dataPoint := range s.data {
 		if dataPoint.typ != 0 {
 			checksum += uint64(pointPos * dataPoint.id)
-			pointPos++
 		}
+		pointPos++
 	}
 	return checksum
 }
